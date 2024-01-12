4b214bb30998350e6772d1a76388495877c22905
Felipe Elvas Barbosa <felipe.100062@aluno.faculdadedescomplica.com.br>
2024-01-12 10:46:04 -0300

    atualização aulas html, css e js

---
+++ curso-javascript-pratica-dio/Variaveis e operadores/primeiros-passos.js
@@ -0,0 +1,8 @@
+//como declarar variáveis
+
+let variavel = 10 / 10;
+
+
+
+
+console.log(variavel);
---
+++ curso-javascript-pratica-dio/condicionais/aula.js
@@ -0,0 +1,154 @@
+
+const numero = 11;
+const ehNumeroPar = ((numero % 2 ) === 0)
+if (ehNumeroPar)  {
+	console.log('O número é par');
+
+}else {	
+	console.log('O número é impar');
+}
+
+// const numero = 11;
+// if ((numero % 2 ) === 0) {
+// 	console.log('O número é par');
+
+// }else {	
+// 	console.log('O número é impar');
+// }
+
+
+// const numero = 11;
+
+
+// const numeroDivisivelPor5 = (numero % 5) === 0;
+// console.log(numeroDivisivelPor5)
+// if (numero === 0) {
+// 	console.log('O número é inválido');
+// }else if(numeroDivisivelPor5) {
+//  console.log('sim');
+// }else {
+// 	console.log('não');
+// }
+
+
+
+
+// const numero = 15;
+
+
+// const numeroDivisivelPor5 = (numero % 5) === 0;
+// console.log(numeroDivisivelPor5)
+
+// if (numeroDivisivelPor5) {
+//  console.log('sim');
+// }else {
+// 	console.log('não');
+// }
+
+
+
+
+
+
+// const numero = 12;
+
+
+// const numeroPar = (numero % 2) == '0';
+// console.log(numeroPar)
+
+// if (numeroPar) {
+//  console.log('Par');
+// }else {
+// 	console.log('Impar');
+// }
+
+
+
+
+
+
+// const numero = 12;
+
+
+// const numeroPar = (numero % 2) == '0';
+// console.log(numeroPar)
+
+// if (numeroPar) {
+//  console.log('Par');
+// }
+// if (!numeroPar) {
+// 	console.log('Impar');
+// }
+
+
+
+
+
+// const numero = 11;
+
+
+// const numeroPar = (numero % 2) == '0';
+// console.log(numeroPar)
+
+// if (numeroPar) {
+//  console.log('Executei');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const numero = 2;
+
+
+// const numeroPar = (numero % 2) === '0';
+// console.log(numeroPar)
+
+
+
+
+
+
+
+
+// const numero = 10;
+
+
+// const numeroPar = (numero % 2) == '0';
+// console.log(numeroPar)
+
+
+
+
+
+
+
+
+
+// const numero = 10;
+
+
+// const numeroPar = (numero % 2) === 0;
+// console.log(numeroPar)
+
+
+
+
+
+
+
+
+
+
+// const camisetaRenanAzul = true;
+// const comisetaLemaoAzul = false;
+
+// 10 >=10
