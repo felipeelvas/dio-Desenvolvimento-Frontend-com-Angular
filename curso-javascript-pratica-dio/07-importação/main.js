4cc8cde0bb61e97863011de1b17894bdb7e23fa2
Felipe Elvas Barbosa <elvasfelipe@hotmail.com>
2024-01-17 20:26:00 -0300

    importacao conteudo

--- curso-javascript-pratica-dio/07-importação/main.js
+++ curso-javascript-pratica-dio/07-importação/main.js
@@ -5,6 +5,7 @@ print(gets());
 
 // const funcoes = require('./funcoes-auxiliares');
 
+
 // // console.log(funcoes.gets());
 
 // const pessoa = {
@@ -12,3 +13,19 @@ print(gets());
 // };
 
 // const nome = pessoa.nome;
+
+ /**Uma sala contem 5 alunos e para cada aluno
+  * foi sorteado um número de 1 - 100. Faça um
+  * programa que receba os 5 números sorteados
+  * para os alunos e mostre o maior número 
+  * sorteado.
+  * Dados de entrada
+  * 5
+  * 50
+  * 10
+  * 98
+  * 23
+  * 
+  * Saída:
+  * 98
+  */
