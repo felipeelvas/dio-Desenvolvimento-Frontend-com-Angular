11a180b20fd501a550e0a36fd245a446089e9e15
Felipe Elvas Barbosa <elvasfelipe@hotmail.com>
2024-02-06 09:38:31 -0300

    aula typescript

---
+++ 02-typescript-para-angular/src/index.ts
@@ -0,0 +1,14 @@
+type heroi={
+    name: string;
+    vulgo: string;
+};
+// isso printa um objeto
+function printaObjetos(pessoa: heroi){
+    console.log(pessoa);
+}
+
+printaObjetos({
+    name: "Bruce Wayne",
+    vulgo: "Batman",
+});
+
