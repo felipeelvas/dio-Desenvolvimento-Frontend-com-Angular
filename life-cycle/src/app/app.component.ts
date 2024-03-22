0deb73676ea548012ccd5bfdcddfc034f71493a6
Felipe Elvas Barbosa <felipe.100062@aluno.faculdadedescomplica.com.br>
2024-03-21 23:52:59 -0300

    life cycle

--- life-cycle/src/app/app.component.ts
+++ life-cycle/src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TitleComponent } from "./title/title.component";
-import { CheckSampleComponent } from './check-sample/CheckSampleComponent';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, TitleComponent, CheckSampleComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'life-cycle';
-  isAliveCheckSample:boolean = true
-
-  disposeCheckSample():void{
-    this.isAliveCheckSample=false
-  }
 }
